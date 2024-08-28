c8ca28745da4d291d96157c32a3f03e0095d2681
Rafa <kimtoba890@gmail.com>
2024-08-28 17:27:33 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,22 @@
+# Posso comentar usando: "#"
+# Continuando na linha de baixo
+
+print ("Rafael") # Posso fazer uma explicação na linha de codigo
+# A linguagem python aceita o '' tambem. Assim podem colocar o apelido da pessoa
+# no codigo. Ex: print ('Rafael "Rafa" Galvão') ou print(" Rafael 'Rafa' Galvão")
+print ('Rafael')
+print ('Rafael "Rafa" Galvão')
+print ("Rafael 'Rafa' Galvão")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
