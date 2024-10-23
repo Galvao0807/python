bc7dcc009c0175fec6c0a7897ce392de3618e4d9
Rafa <kimtoba890@gmail.com>
2024-10-23 15:38:57 -0300

    Update and rename 21-contagem-celulas.py to 21-contagem_de_cedulas_while.py

---
+++ 21-contagem_de_cedulas_while.py
@@ -0,0 +1,23 @@
+valor = int(input("Digite o valor a pagar: \n\n"))
+cedulas = 0
+atual = 50
+apagar = valor
+
+while True
+  if atual <= apagar:
+    apagar -= atual
+    cedulas += 1
+  else:
+    print(f"{cedulas} cédula(s) de R${atual:.2f}")
+    if apagar == 0:
+      break
+    if atual == 50:
+      atual = 20
+    elif atual == 20:
+      atual = 10
+    elif atual == 10:
+      atual = 5
+    elif atual == 5:
+      atual = 1
+      
+    cedulas = 0
